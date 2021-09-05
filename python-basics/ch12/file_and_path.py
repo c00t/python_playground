0606b6b23a0900ae6c0866a361956f508247d979
Eric Zhu <cupofc0t@gmail.com>
2021-09-05 23:05:58 +0800

    12.2 complete

--- python-basics/ch12/file_and_path.py
+++ python-basics/ch12/file_and_path.py
@@ -11,3 +11,21 @@ rel_path = pathlib.Path("/Users/sh33p/")
 print(rel_path.is_absolute())
 
 print(rel_path.resolve())
+
+print(type(some_path.parents))
+# <class 'pathlib._PathParents'>
+
+# check what is `.is_dir()` return if directory not exist, or is a file?
+not_exist_path = pathlib.Path("C:/Users/123")
+a_file_path = pathlib.Path("C:/Users/sh33p/.ssh/id_rsa.pub")
+
+print(not_exist_path.is_dir())
+print(a_file_path.is_dir())
+
+print("--Below are exercises--")
+file_path = pathlib.Path.home() / "my_folder/my_file.txt"
+print(file_path)
+print(f"Existed or not?{file_path.exists()}")
+print("Name is:" + file_path.name)
+print("Parent folder name:" + file_path.parent.name)
+
